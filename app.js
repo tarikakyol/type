4286a22e8400e974f1ae7c682c389e19db2fa138
Tarik <akyoltarik@gmail.com>
2014-09-03 10:39:52 +0300

    x

--- app.js
+++ app.js
@@ -76,15 +76,10 @@ var server = app.listen(port, function() {
 
 var ws = new WebSocket('ws://chat.curateng.com/ws');
 ws.on('open', function() {
-    console.log('connected');
-    ws.send(Date.now().toString(), {mask: true});
-});
-ws.on('close', function() {
-    console.log('disconnected');
-});
-ws.on('message', function(data, flags) {
-    console.log('Roundtrip time: ' + (Date.now() - parseInt(data)) + 'ms', flags);
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 500);
+    console.log('!!!!!!connected');
+    ws.send('something', function(error) {
+        console.log(error);
+        // if error is null, the send has been completed,
+        // otherwise the error object will indicate what failed.
+    });
 });
