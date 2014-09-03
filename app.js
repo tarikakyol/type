a3c42db5d60f747c8c5de06fab68d6674335706e
Tarik <akyoltarik@gmail.com>
2014-09-03 10:26:13 +0300

    websockets added

--- app.js
+++ app.js
@@ -73,8 +73,7 @@ var server = app.listen(port, function() {
     console.log('Splash Chat app %d', server.address().port);
 });
 
-console.log(server.address());
-var wss = new WebSocket({server: server.address()});
+var wss = new WebSocket({server: 'ws://chat.curateng.com'});
 console.log('websocket server created');
 wss.on('connection', function(ws) {
   var id = setInterval(function() {
