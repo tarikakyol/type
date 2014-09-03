2a09384a5370c9eafbc9973c22a74638f77b14a7
Tarik <akyoltarik@gmail.com>
2014-09-03 10:12:31 +0300

    memchachier added final

--- app.js
+++ app.js
@@ -71,3 +71,18 @@ var port = Number(process.env.PORT || 3000);
 var server = app.listen(port, function() {
     console.log('Splash Chat app %d', server.address().port);
 });
+
+var wss = new WebSocketServer({server: server});
+console.log('websocket server created');
+wss.on('connection', function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  });
+  }, 1000);
+
+  console.log('websocket connection open');
+
+  ws.on('close', function() {
+    console.log('websocket connection close');
+    clearInterval(id);
+  });
+});
