9333c36cf968433387877c41dbb23a5ab9e09f16
Tarik <akyoltarik@gmail.com>
2014-09-03 09:06:09 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+
+// prevent Heroku from idling by requesting self in periods
+var request = require('request');
+
+var chat = [];
+var channel = 'default';
+var colors = [];
+var escapeHtml = function(text) {
+  var map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;'
+  };
+  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+app.get("/clear", function(req, res) {
+    chat = [];
+    res.send();
+});
+
+app.get("/changeColor", function(req, res) {
+    req.query.color = req.query.color.replace('hash','#');
+    colors[req.query.nick] = req.query.color;
+    res.send();
+});
+
+app.get("/put", function(req, res) {
+    if(typeof chat[req.query.channel] == "undefined") chat[req.query.channel] = [];
+    if(typeof colors[req.query.nick] == "undefined"){
+        colors[req.query.nick] = "#"+Math.floor(Math.random()*16777215).toString(16);
+    }
+    chat[req.query.channel].push([escapeHtml(req.query.nick), escapeHtml(req.query.text), colors[req.query.nick]]);
+    res.send();
+});
+
+app.get("/get", function(req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send(chat[req.query.channel]);
+});
+
+// hands back a report about the last sync attempt
+app.get("/", function(req, res) {
+    //if(res.query.channel) channel = res.query.channel;
+    res.sendfile('index.html');
+});
+
+// start the Node.js server listening on port 3000
+var port = Number(process.env.PORT || 3000);
+var server = app.listen(port, function() {
+    console.log('Splash Chat app %d', server.address().port);
+});
