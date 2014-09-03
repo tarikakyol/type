cdb71ce9d0f973e64c45fbc36d636cd6bbd489f8
Tarik <akyoltarik@gmail.com>
2014-09-03 10:35:52 +0300

    x

--- app.js
+++ app.js
@@ -74,17 +74,17 @@ var server = app.listen(port, function() {
 });
 
 
-var wss = new WebSocket('ws://chat.curateng.com/ws');
-console.log('websocket server created');
-wss.on('connection', function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  });
-  }, 1000);
-
-  console.log('websocket connection open');
-
-  ws.on('close', function() {
-    console.log('websocket connection close');
-    clearInterval(id);
-  });
+var ws = new WebSocket('ws://chat.curateng.com/ws');
+ws.on('open', function() {
+    console.log('connected');
+    ws.send(Date.now().toString(), {mask: true});
+});
+ws.on('close', function() {
+    console.log('disconnected');
+});
+ws.on('message', function(data, flags) {
+    console.log('Roundtrip time: ' + (Date.now() - parseInt(data)) + 'ms', flags);
+    setTimeout(function() {
+        ws.send(Date.now().toString(), {mask: true});
+    }, 500);
 });
