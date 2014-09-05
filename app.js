e1ff670d28e0c6537875b358affb15fa4c3057bf
Tarik <akyoltarik@gmail.com>
2014-09-05 15:21:08 +0300

    linkify added

--- app.js
+++ app.js
@@ -26,6 +26,23 @@ var escapeHtml = function(text) {
   };
   return text.replace(/[&<>"']/g, function(m) { return map[m]; });
 }
+var linkify = function (inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+    return replacedText;
+}
+var processText = function(text){
+    text = escapeHtml(text);
+    text = linkify(text);
+}
 
 app.use("/public", express.static(__dirname + '/public'));
 
@@ -58,12 +75,12 @@ app.get("/put", function(req, res) {
             }else{
                 chat[req.query.channel] = JSON.parse(val.toString());
             }
-            chat[req.query.channel].push([escapeHtml(req.query.nick), escapeHtml(req.query.text), colors[req.query.nick]]);
+            chat[req.query.channel].push([escapeHtml(req.query.nick), processText(req.query.text), colors[req.query.nick]]);
             mc.set(req.query.channel, JSON.stringify(chat[req.query.channel]));
             res.send();
         }); 
     }else{
-        chat[req.query.channel].push([escapeHtml(req.query.nick), escapeHtml(req.query.text), colors[req.query.nick]]);
+        chat[req.query.channel].push([escapeHtml(req.query.nick), processText(req.query.text), colors[req.query.nick]]);
         mc.set(req.query.channel, JSON.stringify(chat[req.query.channel]));
         res.send();
     }
