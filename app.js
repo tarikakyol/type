40a172e0b2407d16d7876e3c6ba3dca8e5e5c0eb
Tarik <akyoltarik@gmail.com>
2014-09-13 22:01:04 +0300

    torrent fixed

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ var searchMedia = function(query, callback){
             for(i=0;i<results.length;i++){
                 if(results[i].seeds < 10) return cb();
                 //TODO: REMOVE files == 1 and allow dosiers 
-                if(results[i].files == 1 && (results[i].category == "Music" || results[i].category == "Movies")) torrents.push(results[i])
+                if((results[i].category == "Music" || results[i].category == "Movies")) torrents.push(results[i])
             }
             getFeasibleTorrents(page+1, cb);
         });
@@ -113,7 +113,6 @@ var searchMedia = function(query, callback){
         var torrent = torrents[Math.floor(Math.random()*torrents.length)]; // get random
         // console.log(torrent);
         torget.download(torrent,{p:__dirname+"/public/downloads/torrents/" + torrent.title.replace(/ /g, '_') + '.torrent'}, function(err, filename){
-            console.log(err);
             if(err) callback(false);
             else callback(filename,torrent.title, torrent.category);
         })
@@ -133,39 +132,61 @@ var downloadMedia = function(torrent, callback){
     var engine = torrentStream(torrent,opts);
     var piecesCounter = 1;
     var piecesLen = 0;
-    var file = {};
+    var file = [];
+    var isExist = false;
 
-    engine.on('ready', function() {
-        engine.files.forEach(function(f) {
-
-            fs.exists(opts.path+"/"+f.name, function(exists) {
-                console.log(f.name); //TODO: CHECK IF FILE EXTENSION HAVE .mp3 or .mp4 if not return false 
+    engine.files.forEach(function(f) {
+        fs.exists(opts.path+"/"+f.path, function(exists) {
+            var ext = getExtension(f.name).toLowerCase();
+            console.log(ext)
+            if(exists && (ext == ".mp4" || ext == ".mp3")) {
                 file = {
                     filename: f.name,
-                    path: opts.filesDir+"/"+encodeURIComponent(f.name)
+                    path: opts.filesDir+"/"+encodeURIComponent(f.path)
                 }
-                if (exists) {
-                    engine.remove(true, function(){
-                        callback(file);
-                    });
-                }else{
-                    piecesLen = engine.torrent.pieces.length;
-                    var stream = f.createReadStream();
-                }
-            });
-
-            // stream.on('data', function(data) {
-            //     console.log(data);
-            // }); 
-
-            // res.writeHead(200, {"Content-Type" : "audio/mp3"});
-            // stream.pipe(res);
-
+                engine.remove(true, function(){});
+                callback(file);
+                isExist = true;
+                return;
+            }
         });
     });
 
+    if(isExist) return;
+
+    var isExtension = false;
+    engine.files.forEach(function(f) {
+        var ext = getExtension(f.name);
+        if(ext == ".mp4" || ext == ".mp3"){
+            isExtension = true;
+            f.select();
+            piecesLen = engine.torrent.pieces.length;
+            file = {
+                filename: f.name,
+                path: opts.filesDir+"/"+encodeURIComponent(f.path)
+            }
+        }
+    });
+
+    if(!isExtension){
+        engine.remove(true, function(){});
+        callback(false)
+        return;
+    }
+
+    engine.on('ready', function() {
+
+        // stream.on('data', function(data) {
+        //     console.log(data);
+        // }); 
+
+        // res.writeHead(200, {"Content-Type" : "audio/mp3"});
+        // stream.pipe(res);
+
+    });
+
     engine.on('download', function(index){
-        // console.log(piecesLen+"/"+piecesCounter);
+        console.log(piecesLen+"/"+piecesCounter);
         if(piecesLen == piecesCounter) finished();
         else
             piecesCounter++;
@@ -177,6 +198,17 @@ var downloadMedia = function(torrent, callback){
         });
     }
 }
+
+function getExtension(url) {
+    return (url = url.substr(1 + url.lastIndexOf("/")).split('?')[0]).substr(url.lastIndexOf("."))
+}
+
+// searchMedia("Pharrell happy", function(filename, title, category){
+//     console.log(filename);
+//     downloadMedia(fs.readFileSync(filename), function(file){
+//         console.log(file);
+//     })
+// })
 
 app.use("/public", express.static(__dirname + "/public"));
 
