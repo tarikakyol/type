84e56b8fe684cd6a5120636aac2b784c1e250804
Tarik <akyoltarik@gmail.com>
2014-10-07 00:37:45 +0300

    socket logic improved polling (timeinterval) removed

--- public/js/app.js
+++ public/js/app.js
@@ -192,7 +192,6 @@
     }
 
     App.print = function(data) {
-        console.log(data);
         if(App.chatLen == 0 && App.channel == "default")  App.chatLen = data.chatLen; // prevent chat history on default channel
         var dif = data.chatLen - App.chatLen;
         if(dif != 0){
@@ -559,13 +558,10 @@
 
         App.socket.on('connect',function() {
             App.socketOpened();
-            // TODO: move setInterval in nodeJS server-side HARD TO DO BECAUSE ONLINE LIST
-            // setInterval(function() {
-                App.sendSocketMessage('fetch', {
-                    channel: App.channel,
-                    nick: App.getNickName()
-                });
-            // }, 1000);
+            App.sendSocketMessage('fetch', {
+                channel: App.channel,
+                nick: App.getNickName()
+            });
         });
         App.socket.on('disconnect',function() {
             console.log("WebSocket closed, restarting..");
